bf6a4acc2f8061c6ccb8d419ed2482cbae5d26a7
Jan Stümmel <info@janstuemmel.de>
2018-06-23 11:50:15 +0200

    fix, removed semicolons

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
 class App extends Component {
   render() {
-    return <div>Hello World!</div>;
+    return <div>Hello World!</div>
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
-module.hot.accept();
+module.hot.accept()
